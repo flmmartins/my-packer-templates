f256d8c13fba82b521760109947a05e0e4ef365c
Fernanda <flmmartins@gmail.com>
2020-05-21 13:07:28 +0200

    Fix eoan source list and add ansible requirement

--- scripts/ubuntu/install_ansible.sh
+++ scripts/ubuntu/install_ansible.sh
@@ -7,3 +7,6 @@ apt-add-repository ppa:ansible/ansible
 # Install Ansible.
 apt -y update
 apt -y install ansible
+
+# Install sshpass for ansible
+apt-get install -y sshpass
--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -2,6 +2,11 @@
 
 export DEBIAN_FRONTEND="noninteractive"
 
+# Somehow although the iso is eoan apt from there still point to disco
+if [ $(lsb_release -rs) == "19.04" ]; then
+  sed -i 's/disco/eoan/g' /etc/apt/sources.list
+fi
+
 # Update the box
 apt-get update -qq > /dev/null
 apt-get dist-upgrade -qq -y > /dev/null
